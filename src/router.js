3814c4d355576ce0f9dec838fd4061ff5540ceba
Kamil Rzyszka <kamil.rzyszka@student.po.edu.pl>
2023-11-29 23:41:05 +0100

    Added products view, list of CPUs, Firestore database support

--- src/router.js
+++ src/router.js
@@ -2,11 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/views/Home.vue'
 import Configurator from '/views/Configurator.vue'
 import About from '/views/About.vue'
+import Products from '/views/Products.vue'
+import CPUlist from './/components/CPUlist.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/configurator', component: Configurator },
-  { path: '/about', component: About }
+  { path: '/about', component: About },
+  { 
+    path: '/products', 
+    component: Products,
+    children: [
+      {path: 'cpu', component: CPUlist}
+    ] 
+  }
 ]
 
 const router = createRouter({
