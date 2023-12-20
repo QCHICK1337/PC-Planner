101e236ab64cfd3d1aefdc8b4d56da5142764b76
Kamil Rzyszka <kamil.rzyszka@student.po.edu.pl>
2023-12-20 14:11:49 +0100

    Refactor CPUlist.vue, add filtering

--- src/components/CPUlist.vue
+++ src/components/CPUlist.vue
@@ -1,7 +1,16 @@
 <template>
-    <component-list :items="state.products" :fields="fields" :filter-categories="filterCategories"
-        v-model:is-collapsed="state.isCollapsed" v-model:sort-by="state.sortBy" v-model:sort-desc="state.sortDesc"
-        @select-item="selectCpu" :itemType="'cpu'" /> 
+    <b-container fluid>
+        <b-row>
+            <b-col cols="12" md="2">
+                <filter-sidebar :filters="filters" @filter-change="applyFilters" />
+            </b-col>
+            <b-col cols="12" md="8">
+                <component-list :items="filteredProducts" :fields="fields" :filter-categories="filterCategories"
+                    v-model:is-collapsed="state.isCollapsed" v-model:sort-by="state.sortBy" v-model:sort-desc="state.sortDesc"
+                    @select-item="selectCpu" :itemType="'cpu'" />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
@@ -9,21 +18,24 @@ import { db } from '../firebase';
 import { collection, onSnapshot, query } from 'firebase/firestore';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
-import { reactive, onMounted } from 'vue';
+import { reactive, onMounted, computed } from 'vue';
 import ComponentList from './ComponentList.vue';
+import FilterSidebar from './FilterSidebar.vue';
 
 export default {
     components: {
         ComponentList,
+        FilterSidebar,
     },
     setup() {
         const store = useStore();
         const router = useRouter();
         const state = reactive({
             products: [],
-            isCollapsed: false, 
-            sortBy: '', 
-            sortDesc: false, 
+            isCollapsed: false,
+            sortBy: '',
+            sortDesc: false,
+            manufacturers: [],
         });
         const selectCpu = (cpu) => {
             store.dispatch('selectCpu', cpu);
@@ -34,24 +46,39 @@ export default {
             const q = query(collection(db, 'cpu'));
             onSnapshot(q, (snapshot) => {
                 state.products = snapshot.docs.map(doc => doc.data());
+                state.manufacturers = [...new Set(state.products.map(product => product.manufacturer))];
             });
         });
 
+        const filters = computed(() => [
+            {
+                name: 'Manufacturer',
+                options: state.manufacturers,
+                selectedOptions: state.manufacturers,
+            },
+        ]);
+
+        const filteredProducts = computed(() => {
+            return state.products;
+        });
+
         return {
-            state, 
+            state,
             selectCpu,
+            filters,
+            filteredProducts,
         };
     },
     data() {
         return {
             fields: [
-                { key: 'image', sortable: false, label: ''},
+                { key: 'image', sortable: false, label: '' },
                 { key: 'name', sortable: true, label: 'Nazwa' },
                 { key: 'core-count', sortable: true, label: 'Ilość rdzeni' },
                 { key: 'price', sortable: true, label: 'Cena' },
                 { key: 'add', label: '' },
             ],
-            filterCategories: [], 
+            filterCategories: [],
         };
     },
 };
