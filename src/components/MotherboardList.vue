098202dda3ef9f436fc9d385f4a004d61739704a
Kamil Rzyszka <kamil.rzyszka@student.po.edu.pl>
2023-12-21 12:47:01 +0100

    Refactor component lists and add filter functionality

--- src/components/MotherboardList.vue
+++ src/components/MotherboardList.vue
@@ -1,7 +1,16 @@
 <template>
-    <component-list :items="state.products" :fields="fields" :filter-categories="filterCategories"
-        v-model:is-collapsed="state.isCollapsed" v-model:sort-by="state.sortBy" v-model:sort-desc="state.sortDesc"
-        @select-item="selectMotherboard" :itemType="'motherboard'" />
+    <b-container fluid>
+        <b-row>
+            <b-col cols="12" md="2">
+                <filter-sidebar :filters="filters" @filter-change="applyFilters" />
+            </b-col>
+            <b-col cols="12" md="10">
+                <component-list :items="filteredProducts" :fields="fields" :filter-categories="filters"
+                    v-model:is-collapsed="state.isCollapsed" v-model:sort-by="state.sortBy" v-model:sort-desc="state.sortDesc"
+                    @select-item="selectMotherboard" :itemType="'motherboard'" />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
@@ -9,12 +18,14 @@ import { db } from '../firebase';
 import { collection, onSnapshot, query } from 'firebase/firestore';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
-import { reactive, onMounted } from 'vue';
+import { reactive, onMounted, computed } from 'vue'; 
 import ComponentList from './ComponentList.vue';
+import FilterSidebar from './FilterSidebar.vue'; 
 
 export default {
     components: {
         ComponentList,
+        FilterSidebar, 
     },
     setup() {
         const store = useStore();
@@ -30,16 +41,50 @@ export default {
             router.push({ name: 'Configurator', params: { cardId: 'motherboards' } });
         };
 
+        const filters = reactive([
+            {
+                name: 'Manufacturer',
+                label: 'Producent',
+                options: [],
+                selectedOptions: [],
+            },
+        ]);
+
         onMounted(() => {
             const q = query(collection(db, 'motherboard'));
             onSnapshot(q, (snapshot) => {
                 state.products = snapshot.docs.map(doc => doc.data());
+
+                filters.forEach(filter => {
+                    const productProperty = filter.name.toLowerCase();
+                    const uniqueValues = [...new Set(state.products.map(product => product[productProperty]))];
+                    filter.options = uniqueValues;
+                    filter.selectedOptions = uniqueValues;
+                });
+            });
+        });
+
+        const applyFilters = (filterName, selectedOptions) => {
+            const filter = filters.find(filter => filter.name === filterName);
+            if (filter) {
+                filter.selectedOptions = selectedOptions;
+            }
+        };
+
+        const filteredProducts = computed(() => {
+            return state.products.filter(product => {
+                return filters.every(filter => {
+                    return filter.selectedOptions.includes(product[filter.name.toLowerCase()]);
+                });
             });
         });
 
         return {
             state,
             selectMotherboard,
+            filters,
+            applyFilters,
+            filteredProducts,
         };
     },
     data() {
