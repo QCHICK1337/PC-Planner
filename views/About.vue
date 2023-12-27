b9090a72eaa75f8e2d0a238007aaf2d66faf4a74
Kamil Rzyszka <kamil.rzyszka@student.po.edu.pl>
2023-12-27 23:12:41 +0100

    Update About page with PC Planner features

--- views/About.vue
+++ views/About.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class="about">
-        <h1>About Page</h1>
-        <p>{{ aboutText }}</p>
+    <div class="about container text-start my-4">
+        <h5 class="mb-3">Stwórz idealną konfigurację komputera</h5>
+        <p class="mb-4">
+            PC Planner to narzędzie internetowe, które pomaga entuzjastom komputerowym i konstruktorom w tworzeniu idealnych
+            konfiguracji PC. Niezależnie od tego, czy jesteś początkującym, czy doświadczonym konstruktorem, PC Planner
+            upraszcza proces, prowadząc Cię przez wybór komponentów, sprawdzanie kompatybilności i szacowanie kosztów.
+        </p>
+        <h5 class="mb-3">Funkcje</h5>
+        <ul class="mb-4">
+            <li class="mb-2">
+                Wybór Komponentów: Wybieraj spośród szerokiej gamy procesorów, kart graficznych, płyt głównych, pamięci RAM,
+                dysków i innych.
+            </li>
+            <li class="mb-2">
+                Sprawdzanie Kompatybilności: PC-Planner automatycznie sprawdza kompatybilność komponentów, aby zapobiec
+                powszechnym problemom.
+            </li>
+            <li class="mb-2">
+                Szacowanie Kosztów: Otrzymaj szacunkowy koszt całkowity swojej konfiguracji podczas dodawania komponentów.
+            </li>
+            <li class="mb-2">
+                Zapisz i Udostępnij: Zapisuj swoje konfiguracje PC do późniejszego wykorzystania i udostępniaj je innym.
+            </li>
+        </ul>
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            aboutText: "This is some placeholder text for the about page.",
-        };
-    },
-};
-</script>
-
-<style scoped>
-.about {
-    margin: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-}
-</style>
+<style scoped></style>
