7f11ee227978cef1bf1a4f4c3801db731c58fa35
Kamil Rzyszka <kamil.rzyszka@student.po.edu.pl>
2024-01-04 16:23:13 +0100

    Update labels and layout in CaseList and FilterSidebar components

--- src/components/FilterSidebar.vue
+++ src/components/FilterSidebar.vue
@@ -4,16 +4,18 @@
     <b-button v-b-toggle="'collapse-filters'" class="mb-2" variant="primary" v-show="!isDesktop"><font-awesome-icon
         icon="filter" /> Filtry</b-button>
     <b-collapse id="collapse-filters" v-model="isCollapseVisible">
-      <b-form-group :label="filter.label" v-for="filter in filters" :key="filter.name">
-        <template #label>
-          <h6 class="mt-3 mb-1">{{ filter.label }}</h6>
-        </template>
-        <b-form-checkbox-group v-model="filter.selectedOptions" stacked>
-          <b-form-checkbox v-for="option in filter.options" :key="option" :value="option">
-            {{ getOptionLabel(option, filter.name) }}
-          </b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
+      <div class="filter-grid">
+        <b-form-group :label="filter.label" v-for="filter in filters" :key="filter.name" class="filter-group">
+          <template #label>
+            <h6 class="mt-3 mb-1">{{ filter.label }}</h6>
+          </template>
+          <b-form-checkbox-group v-model="filter.selectedOptions" stacked>
+            <b-form-checkbox v-for="option in filter.options" :key="option" :value="option">
+              {{ getOptionLabel(option, filter.name) }}
+            </b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        </div>
     </b-collapse>
   </div>
 </template>
@@ -63,3 +65,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.filter-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 1em;
+}
+
+@media (min-width: 768px) {
+  .filter-grid {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
