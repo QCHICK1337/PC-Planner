8744587ca4fb6b1b82b9b0565f3a660ba9bb39ed
Kamil Rzyszka <kamil.rzyszka@student.po.edu.pl>
2024-01-10 15:30:49 +0100

    Add free-brands-svg-icons package and update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,15 @@
 <template>
   <footer class="footer">
     <div class="container">
-    <span class="text-muted">
-        <font-awesome-icon icon="heart" /> Made by K.R.
+      <span class="text-muted">
+        <font-awesome-icon icon="heart" /> Made by Kamil R.
+        <span class="separator">|</span>
+        <a href="https://github.com/QCHICK1337" target="_blank">
+          <font-awesome-icon icon="code-branch" /> GitHub
+        </a>
         <br />
-        <small class="text-muted">2024</small>
-    </span>
+        <small class="text-muted">2023-2024</small>
+      </span>
     </div>
   </footer>
 </template>
@@ -21,5 +25,14 @@ export default {
   background-color: #f8f9fa;
   padding: 20px;
   text-align: center;
+}
+
+.separator {
+  margin: 0 10px;
+}
+
+a {
+  color: inherit;    
+  text-decoration: none; 
 }
 </style>
