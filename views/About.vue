b13a772f5f1da7dc4fedfd14a1eed7acf96fb876
QCHICK <35234463+QCHICK1337@users.noreply.github.com>
2024-07-08 13:30:13 +0200

    feat(i18n): Update translations for About page

--- views/About.vue
+++ views/About.vue
@@ -1,25 +1,15 @@
 <template>
     <div class="about container text-start my-4">
-        <h2 class="text-center my-4 my-md-5">Informacje</h2>
-        <h5 class="mb-3">Stwórz idealną konfigurację komputera</h5>
+        <h2 class="text-center my-4 my-md-5">{{ $t("about.title") }}</h2>
+        <h5 class="mb-3">{{ $t("about.subtitle") }}</h5>
         <p class="mb-4">
-            PC Planner to narzędzie internetowe, które pomaga entuzjastom komputerowym i konstruktorom w tworzeniu idealnych
-            konfiguracji PC. Niezależnie od tego, czy jesteś początkującym, czy doświadczonym konstruktorem, PC Planner
-            upraszcza proces, prowadząc Cię przez wybór komponentów, sprawdzanie kompatybilności i szacowanie kosztów.
+            {{ $t("about.description") }}
         </p>
-        <h5 class="mb-3">Funkcje</h5>
+        <h5 class="mb-3">{{ $t("about.features") }}</h5>
         <ul class="mb-4">
-            <li class="mb-2">
-                Wybór Komponentów: Wybieraj spośród szerokiej gamy procesorów, kart graficznych, płyt głównych, pamięci RAM,
-                dysków i innych.
-            </li>
-            <li class="mb-2">
-                Sprawdzanie Kompatybilności: PC-Planner automatycznie sprawdza kompatybilność komponentów, aby zapobiec
-                powszechnym problemom.
-            </li>
-            <li class="mb-2">
-                Szacowanie Kosztów: Otrzymaj szacunkowy koszt całkowity swojej konfiguracji podczas dodawania komponentów.
-            </li>
+            <li class="mb-2">{{ $t("about.featureList.componentSelection") }}</li>
+            <li class="mb-2">{{ $t("about.featureList.compatibilityCheck") }}</li>
+            <li class="mb-2">{{ $t("about.featureList.costEstimation") }}</li>
         </ul>
     </div>
 </template>
